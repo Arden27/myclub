ff5e30364a94a084bb5418b1ea650b48013bfaaa
Arden27 <legolasdreams@gmail.com>
2022-10-17 10:54:06 +0200

    first

---
+++ events/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from .models import Event
+from .models import Venue
+from .models import MyClubUser
+
+#admin.site.register(Event)
+admin.site.register(MyClubUser)
+
+@admin.register(Venue)
+class VenueAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address', 'phone')
+    ordering = ('name',) # '-name' for reverse
+    search_fields = ('name', 'address')
+#admin.site.register(Venue) #insted of @admin.register(Venue) (should be below class)
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    fields = (('name', 'venue'), 'event_date', 'description', 'manager')
+    list_display = ('name', 'event_date', 'venue') 
+    list_filter = ('event_date', 'venue')
+    ordering = ('event_date',)
+
+# Register your models here.
---
+++ events/forms.py
@@ -0,0 +1,49 @@
+from django import forms
+from django.forms import ModelForm
+from .models import Venue, Event
+
+# Create a Venue form
+
+class EventForm(ModelForm):
+    class Meta:
+        model = Event
+        fields = ('name', 'event_date', 'venue', 'manager', 'attendees', 'description')
+        labels = {
+            'name':'',
+            'event_date':'',
+            'venue':'',
+            'manager':'',
+            'attendees':'',
+            'description':'',
+        }
+            
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder':'Event Name'}), 
+            'event_date': forms.TextInput(attrs={'class': 'form-control', 'placeholder':'Event Date'}), 
+            'venue': forms.TextInput(attrs={'class': 'form-control', 'placeholder':'Venue'}), 
+            'manager': forms.TextInput(attrs={'class': 'form-control', 'placeholder':'Manager'}), 
+            'attendees': forms.EmailInput(attrs={'class': 'form-control', 'placeholder':'Attendees'}),
+            'description': forms.TextInput(attrs={'class': 'form-control', 'placeholder':'Decription'}), 
+        }
+
+class VenueForm(ModelForm):
+    class Meta:
+        model = Venue
+        fields = ('name', 'address', 'zip_code', 'phone', 'web', 'email_address')
+        labels = {
+            'name':'',
+            'address':'',
+            'zip_code':'',
+            'phone':'',
+            'web':'',
+            'email_address':''
+        }
+            
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder':'Venue Name'}), 
+            'address': forms.TextInput(attrs={'class': 'form-control', 'placeholder':'Address'}), 
+            'zip_code': forms.TextInput(attrs={'class': 'form-control', 'placeholder':'zeipcode'}), 
+            'phone': forms.TextInput(attrs={'class': 'form-control', 'placeholder':'phone'}), 
+            'web': forms.TextInput(attrs={'class': 'form-control', 'placeholder':'web address'}), 
+            'email_address': forms.EmailInput(attrs={'class': 'form-control', 'placeholder':'email',})
+        }
