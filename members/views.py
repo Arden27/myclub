7d379e57703a2d87d66b2d7b88f7d415d14733ad
Arden27 <legolasdreams@gmail.com>
2022-10-21 12:50:40 +0200

    user registration added

--- members/views.py
+++ members/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
+from .forms import RegisterUserForm
+
+def register_user(request):
+    if request.method == 'POST':
+        form = RegisterUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, ('Registration SUccessful!'))
+            return redirect('home')
+    else:
+        form = RegisterUserForm()
+    return render(request, 'authenticate/register_user.html', {
+        'form': form,
+        })
 
 def login_user(request):
     if request.method == 'POST':
